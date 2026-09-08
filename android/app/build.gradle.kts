ede8be021df629278568ed9ba9de230e21f9a421
sadra zadeh khameneh <sadra.z.kh@hotmail.com>
2026-09-09 01:53:44 +0330

    v1.3.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = (System.getenv("VERSION_CODE") ?: "1").toInt()
-        versionName = System.getenv("VERSION_NAME") ?: "1.2.0"
+        versionName = System.getenv("VERSION_NAME") ?: "1.3.0"
         ndk { abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64") }
     }
 
